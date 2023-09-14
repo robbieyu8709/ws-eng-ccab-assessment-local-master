635dea98d70b77ea664787af0eaa04bb9470ad96
Robbie Yu <robbieyu@Robbies-MacBook-Pro.local>
2023-09-14 16:51:35 -0400

    add test

--- backend/test.ts
+++ backend/test.ts
@@ -1,6 +1,7 @@
 import { performance } from "perf_hooks";
 import supertest from "supertest";
 import { buildApp } from "./app";
+import { expect } from "chai";
 
 const app = supertest(buildApp());
 
@@ -15,8 +16,38 @@ async function basicLatencyTest() {
     console.log(`Latency: ${performance.now() - start} ms`);
 }
 
+async function concurrencyTest() {
+    await app.post("/reset").send({ account: "test" }).expect(204);
+
+    const responses = await Promise.all([
+        app.post("/charge").send({ account: "test", charges: 10 }).expect(200),
+        app.post("/charge").send({ account: "test", charges: 20 }).expect(200),
+        app.post("/charge").send({ account: "test", charges: 30 }).expect(200),
+    ]);
+
+    responses.forEach(response => {
+        expect(response.body.isAuthorized).to.be.true;
+    });
+    expect(responses[2].body.remainingBalance).to.equal(40);
+
+    await app.post("/reset").send({ account: "test" }).expect(204);
+
+    const failResponses = await Promise.all([
+        app.post("/charge").send({ account: "test", charges: 30 }).expect(200),
+        app.post("/charge").send({ account: "test", charges: 40 }).expect(200),
+        app.post("/charge").send({ account: "test", charges: 50 }).expect(200),
+    ]);
+
+    // Asserting the responses
+    expect(failResponses[0].body.isAuthorized).to.be.true;
+    expect(failResponses[1].body.isAuthorized).to.be.true;
+    expect(failResponses[2].body.isAuthorized).to.be.false;
+    expect(failResponses[2].body.remainingBalance).to.equal(30);
+}
+
 async function runTests() {
     await basicLatencyTest();
+    await concurrencyTest();
 }
 
 runTests().catch(console.error);
